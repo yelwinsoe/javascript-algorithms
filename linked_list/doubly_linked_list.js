43fba59114c85ad40f90673360051934d932185b
ye <yelwinsoe.mm@gmail.com>
2022-04-18 14:45:30 +0800

    added shift

--- linked_list/doubly_linked_list.js
+++ linked_list/doubly_linked_list.js
@@ -15,7 +15,8 @@ class DoublyLinkedList {
     this.length = 0
   }
 
-  // push
+  // push - adding a new item to the end of the list
+  // Pseudocode
   // create a new node to add in
   // if the list is empty new node is both head and tail
   // else set the next property of the tail to be the new node
@@ -37,7 +38,8 @@ class DoublyLinkedList {
     return this
   }
 
-  // pop
+  // pop - remove an item from the end of the list
+  // Pseudocode
   // if the dll is empty return undefined
   // store a current tail in a temporary variable
   // if the length of dll is 1 then just update head and tail as null
@@ -60,10 +62,35 @@ class DoublyLinkedList {
     return currentTail
   }
 
-  // shift
+  // shift - remove an item from the beginning of the list
+  // Pseudocode
+  // if empty dll, return undefined
+  // create new variable (prevHead) to store current head
+  // if length is 1 then update head and tail to null
+  // else set head to current head.next
+  // set head.prev to null
+  // reduce length
+  // remove next and prev from prevHead
+  // return prevHead
+  shift () {
+    if (!this.head) return undefined
+    let prevHead = this.head
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.head = prevHead.next
+      this.head.prev = null
+    }
+    this.length--
+    prevHead.next = null
+    prevHead.prev = null
+    return prevHead
+  }
   
 
-  // unshift - adding a new node as the first item
+  // unshift - adding a new node at the beginning of the list
+  // Pseudocode
   // create new node
   // if empty dll, add new node as head and tail
   // else update current head prev property to new node
@@ -84,10 +111,11 @@ class DoublyLinkedList {
   }
 
 
-  // get
-  // set
-  // insert
-  // removereverse
+  // get - get an item by index from the list
+  // set - update an item by the index
+  // insert - insert an item to the specific index
+  // remove - remove an item by the index
+  // reverse - reverse the list
 }
 
 let dll = new DoublyLinkedList()
@@ -102,8 +130,16 @@ dll.push('3')
 console.log(dll)
 
 // Shift test
-dll.unshift(4)
+console.log(dll.shift())
 console.log(dll)
+console.log(dll.shift())
+console.log(dll)
+console.log(dll.shift())
+console.log(dll)
+
+// Unshift test
+// dll.unshift(4)
+// console.log(dll)
 
 // // Test pop
 // dll.pop()
